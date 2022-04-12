06f379c82a31ee5b450cf060efd4d7ab97616831
刘久武 <jiuwu.liu@thoughtworks.com>
2022-04-12 21:16:12 +0800

    init

---
+++ dtle/data/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `demo_tbl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
